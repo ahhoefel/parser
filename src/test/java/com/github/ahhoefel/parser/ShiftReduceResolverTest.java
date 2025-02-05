110bdb624b3897abeeaf308cb7b6f81eb866c494
Andrew Hoefel <andrew.hoefel@gmail.com>
2025-02-04 19:18:28 -0600

    Adds LRItemTest and ShiftReduceResolverTest tests

---
+++ src/test/java/com/github/ahhoefel/parser/ShiftReduceResolverTest.java
@@ -0,0 +1,22 @@
+package com.github.ahhoefel.parser;
+
+import com.github.ahhoefel.parser.lang.Rule;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShiftReduceResolverTest {
+
+    @Test
+    public void basicTest() {
+        SymbolTable.TerminalTable terminals = new SymbolTable.TerminalTable();
+        SymbolTable.NonTerminalTable nonTerminals = new SymbolTable.NonTerminalTable();
+        Symbol t = terminals.newSymbol("t");
+        Symbol nt = nonTerminals.newSymbol("T");
+        Rule.Builder rules = new Rule.Builder();
+        Rule r = rules.add(nt, t);
+        ShiftReduceResolver resolver = new ShiftReduceResolver();
+        resolver.addShiftPreference(r, t);
+
+        Assert.assertEquals(resolver.getPreference(r, t).get(), ShiftReduceResolver.Preference.SHIFT);
+    }
+}
